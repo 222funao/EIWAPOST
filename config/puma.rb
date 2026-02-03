6b8f4ada125a5af89c1170b72f265c01c8eae178
root <root@PECE.localdomain>
2026-02-03 08:31:45 -0500

    bddenuevo

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind on all interfaces so platforms like Railway can reach the app.
+port = ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{port}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
